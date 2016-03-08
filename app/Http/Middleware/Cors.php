ee0e7d6e4db88317f3e174f9fc9cbe08694ad34c
Rijad Zuzo <rijad.zuzo@gmail.com>
2016-03-08 16:11:32 +0100

    Changed Cors.php because errors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -1,33 +1,43 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: rijad
- * Date: 08.03.16
- * Time: 16:02
- */
 
 namespace App\Http\Middleware;
 
 use Closure;
 
-use Illuminate\Contracts\Routing\Middleware;
-use Illuminate\Http\Response;
-
-class Cors implements Middleware
+class Cors
 {
-
     /**
      * Handle an incoming request.
      *
-     * @param \Illuminate\Http\Request $request
-     * @param \Closure $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        $content = $next($request);
-        return (new Response($content))->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin');
+
+        $headers = [
+            'Access-Control-Allow-Origin' => $_SERVER['HTTP_ORIGIN'],
+            // CORS doesn't accept Access-Control-Allow-Origin = * for security reasons
+            //'Access-Control-Allow-Origin'    => '*',
+            'Access-Control-Allow-Methods' => 'GET, POST, OPTIONS, PUT, DELETE',
+            //'Access-Control-Allow-Methods'   => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Max-Age' => '86400',
+            'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With',
+            //'Access-Control-Allow-Headers'   => 'X-Custom-Header, X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-security-token',
+        ];
+
+
+        //Using this you don't need an method for 'OPTIONS' on controller
+        if ($request->isMethod('OPTIONS'))
+            return Response::json('{"method":"OPTIONS"}', 200, $headers);
+
+        // For all other cases
+        $response = $next($request);
+        foreach ($headers as $key => $value)
+            $response->header($key, $value);
+
+        return $response;
     }
 }
